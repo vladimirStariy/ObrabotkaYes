554d3583397d113365b21291a06f05d2f98a5226
Vova Starovoyt <starovoyt@storymode.pro>
2022-09-19 23:43:59 +0300

    Add React

---
+++ ObrabotkaYes.React/Initializer.cs
@@ -0,0 +1,33 @@
+﻿using ObrabotkaYes.DataAcessLayer.Interfaces;
+using ObrabotkaYes.DataAcessLayer.Repositories;
+using ObrabotkaYes.Domain.Entity;
+using ObrabotkaYes.Service.Implementations;
+using ObrabotkaYes.Service.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ObrabotkaYes.React
+{
+    public static class Initializer
+    {
+        public static void InitializeRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<User>, UserRepository>();
+            services.AddScoped<IBaseRepository<Profile>, ProfileRepository>();
+            services.AddScoped<IBaseRepository<OrderPicture>, OrderPictureRepository>();
+            services.AddScoped<IBaseRepository<OrderType>, OrderTypeRepository>();
+            services.AddScoped<IBaseRepository<Category>, CategoryRepository>();
+            services.AddScoped<IBaseRepository<Order>, OrderRepository>();
+
+        }
+
+        public static void InitializeServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAccountService, AccountService>();
+            //services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IProfileService, ProfileService>();
+            services.AddScoped<IOrderTypesService, OrderTypesService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IOrderService, OrderService>();
+        }
+    }
+}
