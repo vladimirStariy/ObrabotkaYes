e615ca5832c1679cc2819cdf9e34f8f01c20a1ea
Владимир Старовойт <starovoyt_1995@mail.ru>
2023-02-08 16:08:47 +0300

    вавап

--- ObrabotkaYes/Controllers/HomeController.cs
+++ ObrabotkaYes/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace ObrabotkaYes.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Price()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
