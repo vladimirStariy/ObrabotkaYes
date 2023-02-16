fba4b35701c276f571c909e208f07ff998d2d97e
Владимир Старовойт <starovoyt_1995@mail.ru>
2023-02-16 15:17:39 +0300

    NeoDesign

--- ObrabotkaYes/Controllers/HomeController.cs
+++ ObrabotkaYes/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ObrabotkaYes.Models;
+using ObrabotkaYes.Service.Implementations;
+using ObrabotkaYes.Service.Interfaces;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace ObrabotkaYes.Controllers
@@ -7,14 +11,18 @@ namespace ObrabotkaYes.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOrderService _orderService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOrderService orderService)
         {
             _logger = logger;
+            _orderService = orderService;
         }
 
         public IActionResult Index()
         {
+            var responseOrders = _orderService.GetOrders();
+            ViewBag.Orders = responseOrders.Result;
             return View();
         }
 
